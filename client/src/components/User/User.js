35cbcfa291ec15f52ff3514ddd34d65c4988d44d
aki <ahsi.aki@gmail.com>
2022-06-28 09:35:14 +0800

    0628 aki 註冊跳轉bug修復&會員檔案CRU

--- client/src/components/User/User.js
+++ client/src/components/User/User.js
@@ -1,26 +1,48 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { Button } from "react-bootstrap"
 import './User.css'
+import axios from 'axios'
+
+// memberGender: "0"
+// memberId: 12
+// memberImgPath: null
+// memberMail: "gina@gmail.com"
+// memberName: "沈吉娜"
+// memberNickName: "GINA"
+// memberPasswd: "gina"
+// memberPhone: "0977444555"
+// registerType: "0"
+// updateDatetime: "2022-06-21 15:59:19"
+
+export default function User(props) {  
 
 
-export default function User(props) {
-
-  // 抓取表單資料使用 & 設立初始值
+ //抓取表單資料使用 & 設立初始值
   const [formData, setFormData] = useState(
     {
-      mail: "",
-      passwd: "",
-      passwdCheck: "",
-      forgetPasswordAns: "",
-      name: "",
-      nickName: "",
-      sex: "",
-      phone: ""
+      // mail: "",
+      // name: "",
+      // nickName: "",
+      // sex: "",
+      // phone: ""
     }
   )
+// 跳轉後即刻渲染資料「初始化」
+  useEffect(()=>{
+    setFormData({
+      mail: props.mail,
+      name: props.name,
+      nickName: props.nickName,
+      sex: props.sex,
+      phone: props.phone
+    })
+  },[props])
 
-  
-
+  // 修改介面觸發：個人資料觸發函式
+const [alertData, setAlertData] = useState(false)
+const alertSwitch = () => {
+  setAlertData(prevAlertData => !prevAlertData)
+}
 
   // 追蹤表單輸入值使用
   function inputHandler(e) {
@@ -31,6 +53,29 @@ export default function User(props) {
     }))
   }
   console.log(formData)
+
+  // 送出內容：個人資料修改
+  function alertProfile (e) {
+    e.preventDefault();
+    axios({
+      method:"POST",
+      url: "http://localhost:5000/member/alertProfile",
+      data:{
+        mail: formData.mail,
+        name: formData.name,
+        nickName: formData.nickName,
+        sex: formData.sex,
+        phone: formData.phone
+      }
+    })
+    .then((res)=>{
+    console.log(res)
+    setAlertData(false)
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
 
 
   return (
@@ -52,57 +97,73 @@ export default function User(props) {
         </div>
         <ul className="user--form">
           <li className="user--item">
-            <label htmlFor="mail">帳號</label>
-            <input
+            <label htmlFor="mail">帳號　</label>
+            <span className="fs-4">{formData.mail}</span>
+            {/* <input
               type="text"
               name="mail"
               id="mail"
               value={formData.mail}
               onChange={inputHandler}
-            />
+            />  */}
           </li>
           <li className="user--item">
-            <label htmlFor="name">姓名</label>
-            <input
+            <label htmlFor="name">姓名　</label>
+            {!alertData && <span className="fs-4">{formData.name}</span> }
+            {alertData && <input
               type="text"
-              name="mail"
-              id="mail"
-              value={formData.mail}
-              onChange={inputHandler} />
+              name="name"
+              id="name"
+              value={formData.name}
+              onChange={inputHandler} 
+              /> }
           </li>
           <li className="user--item">
-            <label htmlFor="nickName">暱稱</label>
-            <input
+          <label htmlFor="nickName">暱稱　</label> 
+            {!alertData && <span className="fs-4">{formData.nickName}</span>}
+            {alertData && <input
               type="text"
               name="nickName"
               id="nickName"
               value={formData.nickName}
-              onChange={inputHandler} />
+              onChange={inputHandler} 
+              /> }
           </li>
           <li className="user--item">
-            <label htmlFor="sex">性別</label>
-            <select
+            <label htmlFor="sex">性別　</label>
+            {!alertData && <span className="fs-4">{formData.sex? `女性`:`男性`}</span> }
+            {alertData && <select
               name="sex"
               id="sex"
               value={formData.sex}
-              onChange={inputHandler} >
+              onChange={inputHandler} 
+              > 
               <option value="1">男性</option>
               <option value="0">女性</option>
-            </select>
+            </select> }
           </li>
           <li className="user--item">
-            <label htmlFor="phone">手機</label>
-            <input
+            <label htmlFor="phone">手機　</label>
+            {!alertData && <span className="fs-4">{formData.phone}</span> }
+            {alertData && <input
               type="text"
               name="phone"
               id="phone"
               value={formData.phone}
-              onChange={inputHandler} />
+              onChange={inputHandler} 
+              /> }
           </li>
         </ul>
         <div className="user--btnBar">
-          <Button className="user--btn--M" size="s" variant="secondary">修改</Button>
-          <Button className="user--btn--M" size="s" variant="secondary">儲存</Button>
+          <Button 
+          className="user--btn--M" 
+          size="s" 
+          variant="secondary"
+          onClick={alertSwitch}
+          >修改
+          </Button>
+
+          <Button className="user--btn--M" size="s" variant="secondary" onClick={alertProfile}>儲存</Button>
         </div>
       </div>
 
--- client/src/pages/userPage/userPage.js
+++ client/src/pages/userPage/userPage.js
@@ -1,19 +1,20 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Navbar from '../../components/Navbar/Navbar'
 import '../../components/User/User.css'
-// import axios from 'axios'
+import axios from 'axios'
 import User from '../../components/User/User'
 import OrderList from '../../components/User/OrderList'
 import Feedback from '../../components/User/Feedback'
 import Coupon from '../../components/User/Coupon'
+// import { NavItem } from 'react-bootstrap'
 // import { loginOrNot } from '../../App'
 // import RainbowCard from '../../components/RainbowCard/RainbowCard'
 
 
 export default function UserPage() {
 
-// const [userData, setUserData] = useState("")
+const [userData, setUserData] = useState({})
 
 
 //0623 aki - 若沒有token則跳轉登入頁
@@ -22,30 +23,37 @@ useEffect(()=>{
   if(!localStorage.token){
     navigate('/login', { replace: true })
   }else{
-    // axios({
-    //   method: "post",
-    //   url: "http://localhost:5000/member/isLogin",
-    //   data:{
-    //     token:localStorage.token
-    //   }
-    // }).then((res) => {
-    //   // console.log(res) //有登入的話，回傳「會員資訊」在res.data[0] ｜ 沒登入則回傳message
-    //   let data = res.data[0];
-    //   setUserData(prevUserData => data)//想辦法把值取出來
+    axios({
+      method: "post",
+      url: "http://localhost:5000/member/isLogin",
+      data:{
+        token:localStorage.token
+      }
+    }).then((res) => {
+      // console.log(res) //有登入的話，回傳「會員資訊」在res.data[0] ｜ 沒登入則回傳message
+      let data = res.data[0];
+      setUserData(data)//想辦法把值取出來
 
-    // }).catch((err) => {
-    //   // console.log(err)
-    // })
+    }).catch((err) => {
+      console.log(err)
+    })
   }
-})
+},[])
 
 
+console.log(userData)
 
   return (
     <div className='wrap'>
       <Navbar />
       <div className='User--container'>
-        <User />
+        <User
+        mail={userData.memberMail}
+        name={userData.memberName}
+        nickName={userData.memberNickName}
+        sex={userData.memberGender}
+        phone={userData.memberPhone}
+        />
         <OrderList />
         <Feedback />
         <Coupon />
