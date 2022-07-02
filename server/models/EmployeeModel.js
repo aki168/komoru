5af151abaaae9ae8bc40b11404b17a4cc4d80ec4
Lumi <w2497imt@gmail.com>
2022-07-02 19:35:04 +0800

    2022-07-02 新增後台登入、判斷是否登入狀態、員工列表、員工CRUD api

--- server/models/EmployeeModel.js
+++ server/models/EmployeeModel.js
@@ -1,1 +1,141 @@
 const db = require("./_ConfigDB");
+
+// 2022-06-30 PG
+// 取得員工 Data By employeeAccount
+// return：({})
+exports.getEmployeeDataByEmployeeAccount = async (employeeAccount) => {
+  return new Promise((resolve, reject) => {
+    let sql =
+      "SELECT *" +
+      "FROM `Employee` " +
+      "WHERE `Employee`.`employee_account` = ?;";
+    let value = employeeAccount;
+    db.con.query(sql, value, (err, rows, fields) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(db.rowDataToCamelData(rows));
+    });
+  });
+};
+
+// 2022-07-02 PG
+// 取得員工資料 dataList
+// return：({})
+exports.getEmployeeDataList = async () => {
+  return new Promise((resolve, reject) => {
+    let sql =
+      "SELECT * " + "FROM `Employee` " + "WHERE `Employee`.`is_invalid` = '1';";
+    db.con.query(sql, (err, rows, fields) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(db.rowDataToCamelData(rows));
+    });
+  });
+};
+
+// 2022-07-02 PG
+// 取得員工資料 Data By employeeId
+// return：({})
+exports.getEmployeeDataByEmployeeId = async (employeeId) => {
+  return new Promise((resolve, reject) => {
+    let sql =
+      "SELECT * " +
+      "FROM `Employee` " +
+      "WHERE `Employee`.`employee_id` = ? " +
+      "AND `Employee`.`is_invalid` = '1';";
+    let value = employeeId;
+    db.con.query(sql, value, (err, rows, fields) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(db.rowDataToCamelData(rows));
+    });
+  });
+};
+
+// 2022-07-02 PG
+// 新增員工資料
+// return：{}
+exports.addEmployee = async (dataList) => {
+  return new Promise((resolve, reject) => {
+    let sql =
+      "INSERT INTO `Employee` " +
+      "(`employee_account`, `employee_passwd`, `employee_name`, `employee_phone`, creator_id, `create_datetime`) " +
+      "VALUES (?, ?, ?, ?, ?, ?);";
+    let value = [
+      dataList.employeeAccount,
+      dataList.employeePasswd,
+      dataList.employeeName,
+      dataList.employeePhone,
+      dataList.operatorEmployeeId,
+      db.getDateTimeNow(),
+    ];
+    db.con.query(sql, value, (err, result) => {
+      if (err) {
+        reject(err);
+      }
+      resolve({
+        status: result.serverStatus,
+        employeeId: result.insertId,
+      });
+    });
+  });
+};
+
+// 2022-07-02 PG
+// 修改員工資料 By employeeId
+// return：{}
+exports.updateEmployeeByEmployeeId = async (dataList) => {
+  return new Promise((resolve, reject) => {
+    let sql =
+      "UPDATE `Employee` SET " +
+      "`employee_account` = ?, `employee_passwd` = ?, `employee_name` = ?, `employee_phone` = ?, `updater_id` = ?, `update_datetime` = ? " +
+      "WHERE `Employee`.`employee_id` = ?;";
+    let value = [
+      dataList.employeeAccount,
+      dataList.employeePasswd,
+      dataList.employeeName,
+      dataList.employeePhone,
+      dataList.operatorEmployeeId,
+      db.getDateTimeNow(),
+      dataList.employeeId,
+    ];
+    db.con.query(sql, value, (err, result) => {
+      if (err) {
+        reject(err);
+      }
+      resolve({
+        status: result.serverStatus,
+      });
+    });
+  });
+};
+
+// 2022-07-02 PG
+// 刪除員工資料 By employeeId
+// return：{}
+exports.delEmployeeByEmployeeId = async (dataList) => {
+  return new Promise((resolve, reject) => {
+    let sql =
+      "UPDATE `Employee` SET " +
+      "`is_invalid` = ?, `updater_id` = ?, `update_datetime` = ? " +
+      "WHERE `Employee`.`employee_id` = ?;";
+    let value = [
+      "0",
+      dataList.operatorEmployeeId,
+      db.getDateTimeNow(),
+      dataList.employeeId,
+    ];
+    db.con.query(sql, value, (err, result) => {
+      if (err) {
+        reject(err);
+      }
+
+      resolve({
+        status: result.serverStatus,
+      });
+    });
+  });
+};
--- server/routes/EmployeeRouter.js
+++ server/routes/EmployeeRouter.js
@@ -8,9 +8,50 @@ const employeeController = require("../controllers/EmployeeController");
 const bodyParser = require("body-parser");
 router.use(bodyParser.json());
 
+// 2022-06-30 PG
+// 後台登入所需
+
+const session = require("express-session");
+
+router.use(
+  session({
+    // 加密
+    secret: "kmr",
+    resave: false,
+    saveUninitialized: true,
+    // 預設十分鐘過期
+    cookie: { maxAge: 10000  }
+  })
+);
+
 // ----------------------------------------------------------------------------------------------------------------------
 
+// 2022-06-30 PG
+// 後台登入
+router.post("/login", employeeController.login);
 
+// 2022-07-02 PG
+// 檢查後台使用者是否登入
+router.post("/checkIsLogin", employeeController.checkIsLogin);
 
+// 2022-07-02 PG
+// 取得員工資料 dataList
+router.post("/getEmployeeDataList", employeeController.getEmployeeDataList);
+
+// 2022-07-02 PG
+// 取得員工資料 Data By employeeId
+router.post("/getEmployeeDataByEmployeeId", employeeController.getEmployeeDataByEmployeeId);
+
+// 2022-07-02 PG
+// 新增員工資料
+router.post("/addEmployee", employeeController.addEmployee);
+
+// 2022-07-02 PG
+// 修改員工資料 By employeeId
+router.post("/updateEmployeeByEmployeeId", employeeController.updateEmployeeByEmployeeId);
+
+// 2022-07-02 PG
+// 刪除員工資料 By employeeId
+router.post("/delEmployeeByEmployeeId", employeeController.delEmployeeByEmployeeId);
 
 module.exports = router;
