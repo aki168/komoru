2feba4ff5829ae60143b171e15857c295deeee92
mm3248965 <107457972+mm3248965@users.noreply.github.com>
2022-07-18 08:01:50 +0800

    2022-07-18 Matt 首頁

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -39,7 +39,7 @@ export default function Home() {
             <div className='about'>
               <h2 style={{ display: 'block', color: '#ED8C4E', textAlign: 'center' }}>關於 KOMORU</h2>
               <br />
-              <p className='aboutWords' >ＫＯＭＯＲＵ 為全台最大的複合式訂房平台集結下訂房源、客製活動以及會員中心，為旅者打造前所未有的放假體驗。我們遂將休假與生活結合，創造一個經由住宿來體驗你不曾接觸的活動，期望旅客放假過後能找到新的興趣、甚至改變生活的型態。在 ＫＯＭＯＲＵ 的體驗不僅是休息歇腳等待下次啟程，而是讓你嚐過了一次鮮甜，便期待著下一道菜、下一次的變化！
+              <p className='aboutWords' >ＫＯＭＯＲＵ 為全台最大的複合式訂房平台集結下訂房源、客製活動以及會員中心，為旅者打造前所未有的放假體驗。我們遂將休假與生活結合，創造一個經由住宿來體驗你不曾接觸的活動，期望旅客放假過後能找到新的興趣、甚至改變生活的型態。在 ＫＯＭＯＲＵ 的體驗不僅是休息歇腳等待下次啟程，而是讓你嚐過了一次鮮甜，便期待著下一道菜、下一次的改變。
               </p>
             </div>
             <div >
@@ -55,7 +55,7 @@ export default function Home() {
             </div>
             <h2 style={{ color: '#ED8C4E', textAlign: 'left' }}>合作飯店</h2>
             <br />
-            <p className='RoomWords' >KOMORU為亞洲最大的複合式訂房網集結下定、活動以及員系統為會員系統為及會員系統為一集結下定、活動以及會員系統活動以及會員系統式訂房的複合式訂房網集結下定、活動以及會員系統為一身
+            <p className='RoomWords' >KOMORU合作的飯店遍佈全台，不僅與在地飯店合作、亦活絡當地消費，並且讓旅者與業者達到雙贏！
             </p>
 
             <div className='RoomList' data-aos="fade-up">
@@ -73,7 +73,7 @@ export default function Home() {
               <div className='FeedBackLeft'>
                 <h2 style={{ color: '#ED8C4E', textAlign: 'left' }}>會員系統</h2>
                 <br />
-                <p className='FeedBackWords' >KOMORU擁有易操作多功能的會員系統，協助你輕鬆管理你的會員資料並且瀏覽你的下訂紀錄，探索更多元的自己！
+                <p className='FeedBackWords' >KOMORU擁有易操作多功能的會員系統，協助你輕鬆管理你的會員資料並且瀏覽你的下訂紀錄，探索更多元的自己。
                 </p>
                 
                 <h4 className='FeedBackItem' >• &nbsp;基本資料隨心所欲更換相片
